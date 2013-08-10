7e51863f854f248760036a71ab7dff83ae9fda17
Hack Reactor <hackreactor@Hacks-Mac-mini-3.local>
2013-08-09 17:23:40 -0700

    set passes all tests

--- spec/setSpec.js
+++ spec/setSpec.js
@@ -31,29 +31,24 @@ describe("set", function() {
 
   });
 
-  // it("should return true if a value is in a set", function(){
-  //   set.add("2");
-  //   set.add("chainz");
-  //   set.add(true);
-  //   set.add(4444);
-  //   set.add([1,false,"true"]);
-  //   set.add({what: "why"});
 
+  it("should set values to false when the remove method is called on that value", function(){
+    set.add("2");
+    set.add("chainz");
+    set.add(true);
+    set.add(4444);
+    set.add([1,false,"true"]);
+    set.add({what: "why"});
 
-  // });
+    set.remove("2");
 
+    set.remove(true);
 
-  // it("should set values to false when the remove method is called on that value", function(){
-  //   set.add("2");
-  //   set.add("chainz");
-  //   set.add(true);
-  //   set.add(4444);
-  //   set.add([1,false,"true"]);
-  //   set.add({what: "why"});
+    expect(set.contains(true)).toEqual(false);
 
+    expect(set.contains("2")).toEqual(false);
 
-
-  // });
+  });
 
 
 });
